e70d0f12ced9851d4b5393da55eb8bfc14e1a501
Liat <liatcohen9@gmail.com>
2019-09-06 23:15:46 +0300

    added put route to update client

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,78 +5,51 @@ const axios = require('axios')
 const mongoose = require('mongoose')
 const Client = require('../models/Client')
 
-
-
-// POST NEW CONCERT
-// router.post('/concert', async (req, res) => {
-//     const { artist, date, hour, country, city, venue, num_of_tickets, asked_price, original_price, additional_info, seller, isBid, bid_end_date, bid_end_time } = req.body
-    
-//     const img_url = await findArtistImg(artist)
-//     const newConcert = await sequelize.query(`
-//         INSERT INTO concert ( artist, date, country, city , venue, num_of_tickets, asked_price, original_price, additional_info, seller, status, img_url, uploaded_at, is_bid, ends_at)
-//         VALUES ( '${artist}', '${date} ${hour}:00' , '${country}', '${city}', '${venue}', ${num_of_tickets}, ${asked_price}, ${original_price}, '${additional_info}', ${seller} , 'active', '${img_url}', '${moment().format('YYYY-MM-DD  HH:mm:ss')}', ${isBid}, '${isBid ? `${bid_end_date} ${bid_end_time}:00` : `${date} ${hour}:00`}')
-//         ;`)
-//     res.send(newConcert)
-        
-//     const concertID = newConcert[0]
-//     if(isBid){
-//         const sellerInfo = await fetchSellerInfo(seller)
-//         startCronJob(concertID, bid_end_time, bid_end_date, sellerInfo, req.body);
-//     }
-// })
 router.get('/', function (request, response) {
     response.send("check")
 })
 
 router.get('/clients', async function (req, res) {
     console.log("server get clients")
-    const clients=await Client.find({})
+    const clients = await Client.find({})
     console.log(clients)
-    // console.log(clients)
     res.send(clients)
 })
 
 router.get('/clientsNames', async function (req, res) {
-    const clients=await Client.find({},{name: 1})
+    const clients = await Client.find({}, { name: 1 })
     res.send(clients)
 })
 
 router.get('/owners', async function (req, res) {
-    const clients=await Client.find({},{owner: 1})
+    const clients = await Client.find({}, { owner: 1 })
     res.send(clients)
 })
 router.post('/client', function (req, res) {
     const newClient = new Client({
-                name: req.body.name,
-                email: req.body.email,
-                firstContact: req.body.firstContact,    
-                emailType: req.body.emailType,    
-                sold: req.body.sold,    
-                owner: req.body.owner,
-                country: req.body.country
+        name: req.body.name,
+        email: req.body.email,
+        firstContact: req.body.firstContact,
+        emailType: req.body.emailType,
+        sold: req.body.sold,
+        owner: req.body.owner,
+        country: req.body.country
     })
-   
-    console.log("POST:")
 
+    console.log("POST:")
     console.log(newClient)
     newClient.save()
 })
 
+router.put('/client/:id', async function (req, res) {
+    const query = { "id": req.params.id };
+    const update = { "$set": { [req.body.fieldToUpdate]: req.body.value, } };
 
+    Client.findOneAndUpdate(query, update)
+        .then(
+            res.send(client)
+        ).catch(err => console.error(`Failed to find and update document: ${err}`))
 
-// for (let c of myData){
-//     let newClient = new Client({
-//         name: c.name,
-//         email: c.email,
-//         firstContact: c.firstContact,    
-//         emailType: c.emailType,    
-//         sold: c.sold,    
-//         owner: c.owner,
-//         country: c.country
-//     })
-//     newClient.save()
-// }
-
-
+})
 
 module.exports = router
