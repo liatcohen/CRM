a145a89ae6a3bbbb0fce14958152eec75c7fb831
Liat <liatcohen9@gmail.com>
2019-09-07 19:51:24 +0300

    added chart headline

--- src/components/Analytics/Charts/SalesBarChart.js
+++ src/components/Analytics/Charts/SalesBarChart.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import {
-  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
 
 function SalesBarChart(props) {
 
     return (
+        <div>
+            <div id="chart-headline">sales by country</div>
       <BarChart
-        width={600}
-        height={300}
-        data={props.data}
-        margin={{
-          top: 5, right: 30, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="country" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="sales" fill="#8884d8" />
-      </BarChart>
+                width={600}
+                height={300}
+                data={props.data}
+                margin={{
+                    top: 5, right: 30, left: 20, bottom: 5,
+                }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="country" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="sales" fill="#8884d8" />
+            </BarChart>
+        </div>
+
     );
 }
 
