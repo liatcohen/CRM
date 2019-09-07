9c8b58e7a777fdd938d344c6e72ece4228f5404f
Liat <liatcohen9@gmail.com>
2019-09-07 11:26:14 +0300

    added defaulte values to Update Client inputs

--- src/components/Actions/UpdateClient.js
+++ src/components/Actions/UpdateClient.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import ClientInput from './ClientInput'
+import { conditionalExpression } from '@babel/types';
 const axios = require('axios');
 
 function UpdateClient(props) {
@@ -7,6 +8,7 @@ function UpdateClient(props) {
     const [owner, setOwner] = useState('')
     const [emailType, setEmailType] = useState('')
     const emails = ['A', 'B', 'C', 'D']
+
     function submitSelectedClient(client) {
 
     }
@@ -15,39 +17,66 @@ function UpdateClient(props) {
     //     setOwners(props.owners)
     // }, [])
 
+    const UpdateClient = async (updatedValue, fieldToUpdate) => {
+        console.log("UpdateClient")
+        let clientKey = getClientKey(client)
+        console.log(clientKey)
+        console.log(updatedValue)
+        console.log(fieldToUpdate)
+        await axios.put(`http://localhost:4000/client/${clientKey}`, { "value": updatedValue, "fieldToUpdate": fieldToUpdate })
+    }
+    const getClientKey = (clientName) => {
 
+        return props.clients.find(c => c.name === clientName)._id
+    }
+    function transferOwnership() {
+        console.log("transferOwnership")
+        UpdateClient(owner, 'owner')
+    }
+
+    function sendEmail() {
+        console.log("send email")
+    }
+
+    function declareSale() {
+        console.log("declare sale")
+    }
     return (
         <div className="update-client">
-            <div class="form-style-5">
+            <div className="form-style-5">
                 <form>
                     <fieldset>
-                        <legend><span class="number"></span>Update Client</legend>
+                        <legend><span className="number"></span>Update Client</legend>
                         <div className="update-sub-div choose-client">
                             <label for="client">Client:</label>
-                            <select value={client} onChange={(e) => (setClient(e.target.value))}>
+                            <select placeholder="aaa" value={client} onChange={(e) => (setClient(e.target.value))}>
+                                <option value="" selected disabled hidden>Choose client</option>
+
                                 {props.clients.map(c =>
-                                    <option value={c.name}>{c.name}</option>)}
+                                    <option key={c._id} value={c.name}>{c.name}</option>)}
                             </select>
                         </div>
                         <div className="update-sub-div">
                             <label for="ownership">Transfer ownership to:</label>
                             <select id="ownership" value={owner} onChange={(e) => (setOwner(e.target.value))}>
+                                <option value="" selected disabled hidden></option>
                                 {props.owners.map(ow =>
-                                    <option value={ow.owner}>{ow.owner}</option>)}
+                                    <option key={ow._id} value={ow.owner}>{ow.owner}</option>)}
                             </select>
-                            <input type="submit" value="Transfer" />
+                            <input type="button" value="Transfer" onClick={transferOwnership} />
                         </div>
                         <div className="update-sub-div">
                             <label for="email-type">Send Email:</label>
-                            <select value="emails" onChange={(e) => (setEmailType(e.target.value))}>
+                            <select value={emailType} onChange={(e) => (setEmailType(e.target.value))}>
+                                <option value="" selected disabled hidden></option>
                                 {emails.map(e =>
-                                    <option value={e}>{e}</option>)}
+                                    <option key={e} value={e}>{e}</option>)}
                             </select>
-                            <input type="submit" value="Send" />
+                            <input type="button" value="Send" onClick={sendEmail} />
                         </div>
                         <div className="update-sub-div">
                             <label for="job">Declare Sale</label>
-                            <input type="submit" value="Declare!" />
+                            <input type="button" value="Declare!" onClick={declareSale} />
                         </div>
                     </fieldset>
                 </form>
