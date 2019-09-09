52e868ddd4b895d9e0f2c9d033867e2f4dbaa70f
Liat <liatcohen9@gmail.com>
2019-09-09 14:11:29 +0300

    serch in the db with query

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,33 +8,32 @@ const Client = require('../models/Client')
 router.get('/', function (request, response) {
     response.send("check")
 })
+router.get('/clients/:currentPage?', async function (req, res) {
+    console.log("get clients" )
+    console.log(req.query)
 
-router.get('/clients/:currentPage?/:itemsInPages?', async function (req, res) {
-    // {currentPage: numPage, itemsInPages: 20}
-    console.log("server get clients")
-    let clients = await Client.find({})
-    const totalNumberOfClients= clients.length;
-    const currentPage=req.params.currentPage
-    console.log(currentPage)
-
-    if (currentPage){
-        console.log("currentPage")
-
-        const itemsInPages= parseInt(req.params.itemsInPages)
-        const startItem = (currentPage-1)*itemsInPages
+    let searchCategory=req.query.searchCategory||'name'
+    let searchQuery= req.query.searchQuery ||''
+    searchCategory=searchCategory.toLowerCase()
+    let clients = await Client.find({[searchCategory] : {$regex : `.*${searchQuery}.*`, '$options' : 'i'}})
+    const totalNumberOfClients = clients.length;
+    const currentPage = req.params.currentPage
+    if (currentPage) {
+        const itemsInPages = parseInt(req.query.itemsInPages ||20)
         console.log(itemsInPages)
-        console.log(startItem)
-console.log(startItem+itemsInPages)
-        clients=clients.slice(startItem, startItem+itemsInPages)
-        res.send({clients, totalNumberOfClients })
-
-    }else{
-        res.send({clients})
+        const startItem = (currentPage - 1) * itemsInPages
+        clients = clients.slice(startItem, startItem + itemsInPages)
+        // console.log(clients)
+        res.send({ clients, totalNumberOfClients })
+    } else {
+        res.send({ clients })
     }
-    // console.log(clients)
-    // res.send(clients)
 })
 
+getSearchQuery=(query)=>{
+   let searchCategory=query.searchCategory
+   let searchQuery= query.searchQuery 
+}
 router.get('/clientsNames', async function (req, res) {
     const clients = await Client.find({}, { name: 1 })
     res.send(clients)
@@ -54,24 +53,16 @@ router.post('/client', function (req, res) {
         owner: req.body.owner,
         country: req.body.country
     })
-
-    console.log("POST:")
-    console.log(newClient)
     newClient.save()
 })
 
 router.put('/client/:id', async function (req, res) {
-    console.log("put client")
     const query = { _id: req.params.id };
-    console.log("req.body")
-    // console.log(req.body)
-    // res.send({})
-
     const update = { "$set": { [req.body.fieldToUpdate]: req.body.value, } };
     console.log(query)
     console.log(update)
     Client.findOneAndUpdate(query, update)
-        .then(c => 
+        .then(c =>
             res.send(c)
         ).catch(err => console.error(`Failed to find and update document: ${err}`))
 
